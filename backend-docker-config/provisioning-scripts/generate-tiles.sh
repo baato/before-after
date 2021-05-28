c4852df7cb612d9e8f592d38135b2b836c37eec2
drklrd <srvbhattarai@gmail.com>
2021-05-28 11:14:59 +0545

    use storage to store nodes, ways, relations while generating tiles with --store flag

--- backend-docker-config/provisioning-scripts/generate-tiles.sh
+++ backend-docker-config/provisioning-scripts/generate-tiles.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
 
+media_before=$(date +%Y%m%d%H%M%S)
+media_after=$(date +%Y%m%d%H%M%S)
+
 cd /
-tilemaker /before.osm.pbf  --verbose  --output=/appdata/beforetiles/${5}/
-tilemaker /after.osm.pbf  --verbose  --output=/appdata/aftertiles/${5}/
+tilemaker /before.osm.pbf  --store /tmp/$media_before.media --verbose  --output=/appdata/beforetiles/${5}/
+tilemaker /after.osm.pbf --store /tmp/$media_after.media --verbose  --output=/appdata/aftertiles/${5}/
 
 cd /appdata/beforetiles/${5}/
 gzip -7 -r *
 
 cd /appdata/aftertiles/${5}/
 gzip -7 -r *
+
+rm /tmp/media_before
+rm /tmp/media_after
