4654f461b5331a9551d9f95ce9836de6584dabeb
drklrd <srvbhattarai@gmail.com>
2021-05-28 09:43:35 +0545

    implement frontend app build in docker

--- app-docker-config/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-echo "The platform is available at http://localhost:5678"
-go run /server.go
---
+++ backend-docker-config/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+echo "The platform is available at http://${HOST_IP}:5678"
+go run /server.go
