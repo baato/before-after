0abc5397129c93b24908c772f1385715882eac46
drklrd <srvbhattarai@gmail.com>
2021-06-07 12:29:38 +0545

    fix style web protocol

--- backend/provisioning-scripts/provision.sh
+++ backend/provisioning-scripts/provision.sh
@@ -10,14 +10,14 @@ sed -e 's#_CENTERLAT_MAX_#'"${arr[3]}"'#g' -e 's#_CENTERLAT_MIN_#'"${arr[1]}"'#g
 mkdir -p /appdata/beforestyles/${4}/
 mkdir -p /appdata/afterstyles/${4}/
 
-sed -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"beforetiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/retro.json > /appdata/beforestyles/${4}/retro.json
-sed -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"aftertiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/retro.json > /appdata/afterstyles/${4}/retro.json
+sed -e 's#_HOST_PROTOCOL_#'"${HOST_PROTOCOL}"'#g'  -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"beforetiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/retro.json > /appdata/beforestyles/${4}/retro.json
+sed -e 's#_HOST_PROTOCOL_#'"${HOST_PROTOCOL}"'#g' -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"aftertiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/retro.json > /appdata/afterstyles/${4}/retro.json
 
-sed -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"beforetiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/breeze.json > /appdata/beforestyles/${4}/breeze.json
-sed -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"aftertiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/breeze.json > /appdata/afterstyles/${4}/breeze.json
+sed -e 's#_HOST_PROTOCOL_#'"${HOST_PROTOCOL}"'#g' -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"beforetiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/breeze.json > /appdata/beforestyles/${4}/breeze.json
+sed -e 's#_HOST_PROTOCOL_#'"${HOST_PROTOCOL}"'#g' -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"aftertiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/breeze.json > /appdata/afterstyles/${4}/breeze.json
 
-sed -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"beforetiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/monochrome.json > /appdata/beforestyles/${4}/monochrome.json
-sed -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"aftertiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/monochrome.json > /appdata/afterstyles/${4}/monochrome.json
+sed -e 's#_HOST_PROTOCOL_#'"${HOST_PROTOCOL}"'#g' -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"beforetiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/monochrome.json > /appdata/beforestyles/${4}/monochrome.json
+sed -e 's#_HOST_PROTOCOL_#'"${HOST_PROTOCOL}"'#g' -e 's#_HOSTNAME_#'"${HOST_IP}"'#g' -e 's#_TILE_EPOCH_#'"aftertiles"'#g' -e 's#_UUID_#'"${4}"'#g' /map-styles/monochrome.json > /appdata/afterstyles/${4}/monochrome.json
 
 echo "Provision ready!"
 
