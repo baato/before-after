fc99a81e493f0f75067273f15605d151c03ca308
drklrd <srvbhattarai@gmail.com>
2021-06-07 14:10:49 +0545

    fix issue with concurrent osm file download and extracts generation

--- backend/provisioning-scripts/generate-tiles.sh
+++ backend/provisioning-scripts/generate-tiles.sh
@@ -4,8 +4,8 @@ media_before=$(date +%Y%m%d%H%M%S)
 media_after=$(date +%Y%m%d%H%M%S)
 
 cd /
-tilemaker /before.osm.pbf  --store /tmp/$media_before.media --verbose  --output=/appdata/beforetiles/${4}/
-tilemaker /after.osm.pbf --store /tmp/$media_after.media --verbose  --output=/appdata/aftertiles/${4}/
+tilemaker /extracts/before_${5}-${1}0101.osm.pbf  --store /tmp/$media_before_${5}-${1}0101.media --verbose  --output=/appdata/beforetiles/${4}/
+tilemaker /extracts/after_${5}-latest.osm.pbf --store /tmp/$media_before_${5}-${1}0101.media --verbose  --output=/appdata/aftertiles/${4}/
 
 cd /appdata/beforetiles/${4}/
 gzip -7 -r *
@@ -13,5 +13,5 @@ gzip -7 -r *
 cd /appdata/aftertiles/${4}/
 gzip -7 -r *
 
-rm /tmp/media_before
-rm /tmp/media_after
+rm /tmp/$media_before_${5}-${1}0101.media
+rm /tmp/$media_before_${5}-${1}0101.media
