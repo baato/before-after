17ca606e4ce46f17e08faa6367ac337efafbb548
drklrd <srvbhattarai@gmail.com>
2021-06-15 14:26:19 +0545

    breakdown OSM data into thematic layers

--- backend/provisioning-scripts/generate-tiles.sh
+++ backend/provisioning-scripts/generate-tiles.sh
@@ -1,36 +1,51 @@
 #!/bin/bash 
 
-media_before=$(date +%Y%m%d%H%M%S)
-media_after=$(date +%Y%m%d%H%M%S)
+## declare an array variable for themes you want to include in data
+declare -a arr=("highway=trunk" "highway=primary" "highway=secondary" "highway=tertiary" "highway=unclassifed" "building" "amenity" "landuse" "waterway" "water" "landcover")
 
-osmium tags-filter -o /extracts/before_$4_highway.osm.pbf /extracts/before_${4}.osm.pbf highway
-osmium tags-filter -o /extracts/before_$4_building.osm.pbf /extracts/before_${4}.osm.pbf building 
-osmium tags-filter -i -o /extracts/before_$4_others.osm.pbf /extracts/before_${4}.osm.pbf building highway
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   osmium tags-filter -o /extracts/before_$4_$i.osm.pbf /extracts/before_${4}.osm.pbf nwr/$i
+done
 
-osmium tags-filter -o /extracts/after_$4_highway.osm.pbf /extracts/after_${4}.osm.pbf highway
-osmium tags-filter -o /extracts/after_$4_building.osm.pbf /extracts/after_${4}.osm.pbf building 
-osmium tags-filter -i -o /extracts/after_$4_others.osm.pbf /extracts/after_${4}.osm.pbf building highway
+for i in "${arr[@]}"
+do
+   osmium tags-filter -o /extracts/after_$4_$i.osm.pbf /extracts/after_${4}.osm.pbf nwr/$i
+done
 
 cd /
 
-tilemaker /extracts/before_$4_building.osm.pbf    --output=/appdata/beforetiles/${4}.mbtiles
-tilemaker /extracts/before_$4_highway.osm.pbf --merge --output=/appdata/beforetiles/${4}.mbtiles
-tilemaker /extracts/before_$4_others.osm.pbf --merge --output=/appdata/beforetiles/${4}.mbtiles
+# merge layers one by one
+for i in "${arr[@]}"
+do
+   tilemaker /extracts/before_$4_$i.osm.pbf --merge    --output=/appdata/beforetiles/${4}.mbtiles
+done
+
+# generate tiles from mbtiles
 mb-util --image_format=pbf /appdata/beforetiles/${4}.mbtiles /appdata/beforetiles/${4} 
 
 
-tilemaker /extracts/after_$4_building.osm.pbf    --output=/appdata/aftertiles/${4}.mbtiles
-tilemaker /extracts/after_$4_highway.osm.pbf --merge --output=/appdata/aftertiles/${4}.mbtiles
-tilemaker /extracts/after_$4_others.osm.pbf --merge --output=/appdata/aftertiles/${4}.mbtiles
+for i in "${arr[@]}"
+do
+   tilemaker /extracts/after_$4_$i.osm.pbf --merge    --output=/appdata/aftertiles/${4}.mbtiles
+done
+
 mb-util --image_format=pbf /appdata/aftertiles/${4}.mbtiles /appdata/aftertiles/${4} 
 
-# tilemaker /extracts/after_${4}.osm.pbf --store /tmp/$media_after_${4}.media  --output=/appdata/aftertiles/${4}/
-
+# gzip the generated tiles
 cd /appdata/beforetiles/${4}/
 gzip -7 -r *
 
 cd /appdata/aftertiles/${4}/
 gzip -7 -r *
 
-rm /tmp/$media_before_${4}.media
-rm /tmp/$media_after_${4}.media
+# remove intermediate files
+for i in "${arr[@]}"
+do
+   rm /extracts/before_$4_$i.osm.pbf 
+   rm /extracts/after_$4_$i.osm.pbf 
+done
+
+rm /appdata/beforetiles/${4}.mbtiles
+rm /appdata/aftertiles/${4}.mbtiles
