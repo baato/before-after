472ffa466216941a27cd53ae26533bac39bc987b
drklrd <srvbhattarai@gmail.com>
2022-06-22 10:03:23 +0545

    add system temp unavailable banner

--- frontend/src/app/src/components/NotWorking.vue
+++ frontend/src/app/src/components/NotWorking.vue
@@ -2,17 +2,17 @@
   <v-dialog persistent v-model="dialog" color="green" width="500">
     <v-card>
       <v-card-title style="background-color: #47889d; color: #ffffff">
-        The service is temporarily unavailable!
+        Service Temporarily Unavailable
       </v-card-title>
 
       <v-card-text class="pt-4">
         <p>
-          We are currently having some issues with the before-after maps
-          provisioning system. Our technical team is working on it and it should
-          be resolved soon.
+          We are currently experiencing some issues with the mailserver because
+          of which emails are not being delivered after the before-after maps
+          are generated. We are working to fix this issue as soon as possible.
         </p>
 
-        <p>Please do come back later. Sorry for the inconvenience.</p>
+        <p>Please do come back later. Sorry for the inconvenience!</p>
       </v-card-text>
 
       <v-card-actions>
