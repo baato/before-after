8a536f862c6bd4bbcb6e7efd716b0f55ed0810fc
Aadesh-Baral <elkloppiko@gmail.com>
2023-01-06 11:40:50 +0545

    Add sentry for log capture

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"github.com/baato/before-after/api"
+	"github.com/getsentry/sentry-go"
 )
 
 func main() {
+	// Initialize Sentry
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+		// Enable printing of SDK debug messages.
+		// Useful when getting started or trying to figure something out.
+		Debug: true,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	// Set the timeout to the maximum duration the program can afford to wait.
+	defer sentry.Flush(2 * time.Second)
+
 	server := api.NewServer()
 
 	// Start listening for incoming chat messages
